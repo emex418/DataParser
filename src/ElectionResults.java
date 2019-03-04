680bb70a71ceb361b7c6d18adc379fde1314bb07
emex418 <elizabeth@eckeshome.com>
2019-03-03 21:48:30 -0800

    Added Getters and Setters to ElectionResults

--- src/ElectionResults.java
+++ src/ElectionResults.java
@@ -17,4 +17,85 @@ public class ElectionResults {
         this.combindedFips = combinedFips;
     }
 
+
+    public int getDemVotes() {
+        return demVotes;
+    }
+
+    public void setDemVotes(int demVotes) {
+        this.demVotes = demVotes;
+    }
+
+    public int getGopVotes() {
+        return gopVotes;
+    }
+
+    public void setGopVotes(int gopVotes) {
+        this.gopVotes = gopVotes;
+    }
+
+    public int getTotalVotes() {
+        return totalVotes;
+    }
+
+    public void setTotalVotes(int totalVotes) {
+        this.totalVotes = totalVotes;
+    }
+
+    public int getVotesDifference() {
+        return votesDifference;
+    }
+
+    public void setVotesDifference(int votesDifference) {
+        this.votesDifference = votesDifference;
+    }
+
+    public double getPercentDem() {
+        return percentDem;
+    }
+
+    public void setPercentDem(double percentDem) {
+        this.percentDem = percentDem;
+    }
+
+    public double getPercentGOP() {
+        return percentGOP;
+    }
+
+    public void setPercentGOP(double percentGOP) {
+        this.percentGOP = percentGOP;
+    }
+
+    public double getPercentDifference() {
+        return percentDifference;
+    }
+
+    public void setPercentDifference(double percentDifference) {
+        this.percentDifference = percentDifference;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public int getCombindedFips() {
+        return combindedFips;
+    }
+
+    public void setCombindedFips(int combindedFips) {
+        this.combindedFips = combindedFips;
+    }
+
 }
