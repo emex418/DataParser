aceaef2ff2474c5c35fecdaedadf1398c5d27d7b
emex418 <elizabeth@eckeshome.com>
2019-03-04 00:44:16 -0800

    First Draft of parse2016ElectionResults - not functional, Brute Force

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /*
 Main method for Data Parser
@@ -6,6 +7,10 @@ Author: Elizabeth Eckes 2019
 public class Main {
     public static void main(String[] args) {
         String data = Utils.readFileAsString("data\\2016_Presidential_Results.csv");
-        
+        ArrayList<ElectionResults> results = Utils.parse2016ElectionResults(data);
+        for(ElectionResults r : results){
+            r.toString();
+        }
+
     }
 }
--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -17,5 +18,33 @@ public class Utils {
         }
 
         return output.toString();
+    }
+
+    public static ArrayList<ElectionResults> parse2016ElectionResults(String data) {
+        ArrayList<ElectionResults> electionResults = new ArrayList<>();
+        String[] lines = data.split("\n");
+        for (int i =  1; i < lines.length; i++) {
+            ElectionResults temp = parseLine(lines[i]);
+            electionResults.add(temp);
+        }
+        return electionResults;
+    }
+
+    private static ElectionResults parseLine(String line) {
+        //TODO: change from a brute force solution to a simpler solution - loop solution
+        ElectionResults electionResults = new ElectionResults(0, 0, 0, 0, 0, 0, 0, "", "", 0);
+        String[] columns = line.split(",");
+//        electionResults.setDemVotes((int)Double.parseDouble(columns[1]));
+//        electionResults.setGopVotes((int)Double.parseDouble(columns[2]));
+//        electionResults.setTotalVotes((int)Double.parseDouble(columns[3]));
+//        electionResults.setPercentDem(Double.parseDouble(columns[4]));
+//        electionResults.setPercentGOP(Double.parseDouble(columns[5]));
+//        int votesDifference = (int)Double.parseDouble(columns[6].substring(1) + columns[7].substring(0,columns[7].length()-1));
+//        electionResults.setVotesDifference(votesDifference);
+//        electionResults.setPercentDifference(Double.parseDouble(columns[8].substring(0, columns[8].length()-1)));
+//        electionResults.setState(columns[9]);
+//        electionResults.setCounty(columns[10]);
+//        electionResults.setCombindedFips(Integer.parseInt(columns[11]));
+        return electionResults;
     }
 }
