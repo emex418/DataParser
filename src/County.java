0fa05cc92cb61bcd99f67bb1bce4bb9e23fdb8aa
emex418 <elizabeth@eckeshome.com>
2019-03-10 20:28:18 -0700

    Created Data Manager class along with State and County objects

---
+++ src/County.java
@@ -0,0 +1,8 @@
+public class County {
+    private String name, state;
+    private ElectionData election2016;
+    private EducationalAttainment edu2016;
+    private Unemployent unemployment2016;
+
+    public County(){}
+}
---
+++ src/State.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class State {
+    private String name;
+    private ArrayList<County> counties = new ArrayList<County>();
+
+    public State(){}
+}
