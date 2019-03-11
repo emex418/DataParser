930c8330093ec0a94974dbe531bdaec222d71e91
emex418 <elizabeth@eckeshome.com>
2019-03-10 22:10:39 -0700

    Added a getAllStates method to fill the states arrayList in DataManager

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,9 +1,21 @@
 import java.util.ArrayList;
 
 public class DataManager {
-    private ArrayList<State> states = new ArrayList<State>();
+    private static ArrayList<State> states;
 
     public static void main(String[] args) {
-        //TODO: fill arrayList with states
+        states = getAllStates("data\\Education.csv");
+    }
+
+    private static ArrayList<State> getAllStates(String file) {
+        ArrayList<State> states = new ArrayList<>();
+        String[] counties = file.split("\n");
+        states.add(new State("AL"));
+        for (int i = 7; i < counties.length; i++) {
+            String[] variables = counties[i].split(",");
+            String lastState = states.get(states.size()-1).getName();
+            if (variables[1].equals(lastState)) states.add(new State(variables[1]));
+        }
+        return states;
     }
 }
